b276b1f71280c468fdf29f8a394857c4a37dda6a
Kevin Noguera <noguera.ke@gmail.com>
2024-12-09 21:11:25 -0500

    Define Candidate interface

---
+++ src/interfaces/Candidate.interface.tsx
@@ -0,0 +1,18 @@
+
+interface Candidate {
+  id: number;
+  login: string;
+  avatar_url: string;
+  html_url: string;
+  name?: string;
+  location?: string;
+  email?: string;
+  company?: string;
+  bio?: string;
+
+}
+
+export default Candidate;
+
+
+
